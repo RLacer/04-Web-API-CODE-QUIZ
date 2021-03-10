96296edef22ae4a69cd11fc1d2889246971af76d
Rebecca Lacer <r.a.lacer@hotmail.com>
2021-03-10 12:57:15 -0700

    working on java

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,37 @@
 var startBtnEl = document.getElementById("start");
 var startScreenEl = document.getElementById("startscreen");
 var questionsEl = document.getElementById("questions");
+var timerId;
+var time = questions.length * 15;
+var timerEl = document.getElementById("time");
+var quesIndex = 0;
+
 
 function startQuiz() {
+startScreenEl.setAttribute("class", "hide");
+questionsEl.removeAttribute("class");
+timerId = setInterval(clockTick, 1000);
+timerEl.textContent = time;
+getQuestion();
 
 };
 
+function clockTick() {
+    time--;
+    timerEl.textContent = time;
 
+    if (time <= 0) {
+        // quizEnd();
+    
+    }
+}
 
-
+function getQuestion() {
+    var currentQuestion = questions[quesIndex];
+console.log(currentQuestion);
+    var titleEl = document.getElementById("question_title");
+    titleEl.textContent = currentQuestion.title;
+}
 
 
 
@@ -16,4 +39,5 @@ function startQuiz() {
 
 
 startBtnEl.onclick = startQuiz;
+
 
