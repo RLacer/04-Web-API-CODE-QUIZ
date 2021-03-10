cc655a2501a454b6e525f227c61ccc437a8b6fcf
Rebecca Lacer <r.a.lacer@hotmail.com>
2021-03-10 12:26:20 -0700

    trying again

---
+++ assets/questions.js
@@ -0,0 +1,14 @@
+var questions = [
+    {
+        title: "Javascript files use which file type?",
+        choices: [".js", ".jt", ".tml", "none of these"],
+        answer: ".js"
+       
+    },
+    {
+        title: "You can add javascript to a html by using which of the following?",
+        choices: ["all  of these", "using a script tag in the body of the html", "using a script tag and a link to the current folder", "using a link to another folder"],
+        answer: "all of these"
+
+    }
+]
---
+++ assets/script.js
@@ -0,0 +1,19 @@
+var startBtnEl = document.getElementById("start");
+var startScreenEl = document.getElementById("startscreen");
+var questionsEl = document.getElementById("questions");
+
+function startQuiz() {
+
+};
+
+
+
+
+
+
+
+
+
+
+startBtnEl.onclick = startQuiz;
+
