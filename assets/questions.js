ef95149ee1a09613c981be1725dc65277fc7f9d4
Rebecca Lacer <r.a.lacer@hotmail.com>
2021-03-12 09:26:26 -0700

    added questions

--- assets/questions.js
+++ assets/questions.js
@@ -1,14 +1,55 @@
 var questions = [
     {
         title: "Javascript files use which file type?",
-        choices: [".jt", ".js", ".tml", "none of these"],
-        answer: ".js"
+        choices: [" .jt", " .js", " .tml", "none of these"],
+        answer: " .js"
        
     },
     {
         title: "You can add javascript to a html by using which of the following?",
-        choices: ["using a script tag in the body of the html", "using a script tag and a link to the current folder", "using a link to another folder", "all  of these"],
-        answer: "all of these"
+        choices: [" using a script tag in the body of the html", " using a script tag and a link to the current folder", " using a link to another folder", " all of these"],
+        answer: " all of these"
 
-    }
+    },
+{
+    title: "Which of the following is NOT primitive a data type?",
+    choices: [" numbers", " string", " array", " boolean"],
+    answer: " array"
+   
+},
+
+{
+    title: "Which of the following is not a core language object??",
+    choices: [" array", " character", " date", " math"],
+    answer: " character"
+   
+},
+
+{
+    title: "What does the getElementById use?",
+    choices: [" css", " html", " js", " api"],
+    answer: " html"
+   
+},
+{
+    title: "What does NaN stand for?",
+    choices: [" Now and Next", " Not a Number", " Number and Next", " Number andn Next = True "],
+    answer: " Not a Number"
+   
+},
+{
+    title: "What does the || stand for in the following: length < 8 || length > 128?",
+    choices: [" or", " and", " .if", " else"],
+    answer: " or"
+   
+},
+
+{
+    title: "In javascript, you must use upper case for which of the following?",
+    choices: [" var", " function", " alert", " none of these"],
+    answer: " none of these"
+   
+},
+
+
 ]
